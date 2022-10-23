3e5b2a504f570ae8a0883a244c3746a52c91afc5
Olga Fedorova <bamnu13@gmail.com>
2022-10-23 23:48:51 +0300

    Simplified next in Iterator

--- src/main/java/ru/netology/Randoms.java
+++ src/main/java/ru/netology/Randoms.java
@@ -4,14 +4,13 @@ import java.util.Iterator;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
-    protected Random random;
+    protected Random random = new Random();
     private final int min;
     private final int max;
 
     protected Randoms(int min, int max) {
         this.min = min;
         this.max = max;
-        random = new Random();
         if (min > max) {
             throw new IllegalArgumentException("Первое число 'min' должно быть меньше второго 'max'!");
         }
@@ -31,12 +30,7 @@ public class Randoms implements Iterable<Integer> {
 
         @Override
         public Integer next() {
-            int limit = max + 1;
-            int x = random.nextInt(min, limit);
-            if (x != random.nextInt(min, limit)) {
-                x = random.nextInt(min, limit);
-            }
-            return x;
+            return random.nextInt(min, max + 1);
         }
     }
 }
